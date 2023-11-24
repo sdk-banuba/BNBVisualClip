453b00ad08a3df32925b87261ec1d1f0c55d6c16
ci <ci@banuba.com>
2023-11-24 17:07:41 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBVisualClip",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBVisualClip",
+            targets: [
+                "BNBVisualClip",
+                "BNBVisualClip_BNBSdkCore"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBVisualClip",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBVisualClip.zip",
+            checksum: "3d41b87887dd41ade61679af46c0cdf581e6fd241263e94ea571531d8665f84b"
+        ),
+        .target(
+            name: "BNBVisualClip_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+    ]
+)
