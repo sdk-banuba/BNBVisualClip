0b2c2c2ebe014543d8d9adcdc52f463549d92db5
macOS CI <sdk.accounts@banuba.com>
2024-02-23 18:07:18 +0300

    1.11.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.0-3-gb365c1950"
+let bnbPackageVersion: Version = "1.11.1"
 
 let package = Package(
     name: "BNBVisualClip",
@@ -26,8 +26,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBVisualClip",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.0-3-gb365c1950/BNBVisualClip.zip",
-            checksum: "7ad01a7263482fade1047af50b5758c37294f80ae54a15e2b19eb64a5a957f2d"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1/BNBVisualClip.zip",
+            checksum: "99b7a92c980dc2fbafac75fa733001f24576a5af11d9aa91674a6dbce3d319a7"
         ),
         .target(
             name: "BNBVisualClip_BNBSdkCore",
