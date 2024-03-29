820cd19275f89b1e4d5f39bfb8d63dd13a27a377
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-03-29 09:30:23 +0300

    1.11.1-70-g2e0f1ab74e

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.11.1-70-g2e0f1ab74"
+let bnbPackageVersion: Version = "1.11.1-70-g2e0f1ab74e"
 
 let package = Package(
     name: "BNBVisualClip",
@@ -26,8 +26,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBVisualClip",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.11.1-70-g2e0f1ab74/BNBVisualClip.zip",
-            checksum: "dd2f1292323cc39ae485d6ca7bd65fb1ecc1f40ce727cf3492613233cdd31e30"
+            url: "None",
+            checksum: "fbc64a025571f2768fd810c69e7811a31325de441d1eb238d44f37a9a9e793b9"
         ),
         .target(
             name: "BNBVisualClip_BNBSdkCore",
